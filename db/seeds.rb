5021ffc8add0253835c14b6d1db743253e630a66
Cedric Lang-Roth <dnj855@yahoo.com>
2024-11-28 11:19:20 +0100

    Finish seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'open-uri'
+require 'json'
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+movies = JSON.parse(URI.parse(url).read)
+
+Movie.destroy_all
+
+movies['results'].each do |movie|
+  Movie.create!(
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
